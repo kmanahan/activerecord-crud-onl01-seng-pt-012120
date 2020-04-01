15ab6c4aebaa53a4e092a0c04696acf842d62d50
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-01 01:02:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,7 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   movie = Movie.order(release_date: :desc)
-  movie.where
+  movie.where(release_date)
   end
   
 end
